81d06c0646a3726e21ff016cc7d9e0258ace8a78
KIMHANGIL <khg961009@gmail.com>
2020-09-15 16:47:30 +0900

    이런식

---
+++ SAMPLE/SAMPLE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SAMPLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
