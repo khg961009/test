c3b58c2aa309640c2d138c0df973852e6ecff111
KIMHANGIL <khg961009@gmail.com>
2020-09-15 10:26:26 +0900

    test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello Git!");
             Console.WriteLine("Have good days!");
+            Console.WriteLine("no dd dd!");
         }
     }
 }
